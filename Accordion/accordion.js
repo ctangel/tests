570b62fd90c685e71d2c399852fd052a476b23a1
Luis Gonzalez <luisgonzalez081795@gmail.com>
2013-08-13 19:04:27 -0400

    ordered files

---
+++ Accordion/accordion.js
@@ -0,0 +1,17 @@
+$(function() {
+	var accordion = $("#accordion");
+	var headings = $("h2");
+	var paragraphs = $("p");
+	paragraphs.not(":first").hide();
+	accordion.on("click", "h2", function() {
+		var t = $(this);
+		var tPara = t.next();
+		if(!tPara.is(":visible")) {
+			tPara.trigger("showParagraph");
+		}
+	});
+	accordion.on("showParagraph", "p", function() {
+		paragraphs.stop(true, true).slideUp(400, "easeInCirc");
+		$(this).stop(true, true).slideDown(400, "easeInCirc");
+	});
+});
